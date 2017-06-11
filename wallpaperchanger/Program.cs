886c4945341e083d0dfbc95f1a95cb8deeed9875
jignick <jig.nick@yahoo.gr>
2017-06-11 13:41:26 +0300

    v1.0

---
+++ wallpaperchanger/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace wallpaperChanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wallpaperDirectory = "/home/jig/Pictures/wallpapers/";
+            var randomWallpaper = new Random();
+            var fileNames = Directory.GetFiles(wallpaperDirectory);
+            var indexOfWallpaper = randomWallpaper.Next(0, fileNames.Length-1);
+
+            Process fehStart = new Process();
+            fehStart.StartInfo.FileName = "feh";
+
+            if (fileNames[indexOfWallpaper] != (wallpaperDirectory+"blondebitch.jpg") 
+                && fileNames[indexOfWallpaper] != (wallpaperDirectory+"15.jpg"))
+        {
+                fehStart.StartInfo.Arguments = "--bg-scale "+fileNames[indexOfWallpaper];
+                fehStart.Start();
+        }
+            else
+            {
+                fehStart.StartInfo.Arguments = "--bg-center "+fileNames[indexOfWallpaper];
+                fehStart.Start();
+            }
+         
+
+        }
+    }
+}
